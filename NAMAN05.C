4fcb653ad0ebef13de5b2ea14015d15e5ea17338
PL-I-C-Programming-Naman <naman0009kumar@gmail.com>
2026-08-31 12:59:25 +0530

    Create NAMAN05.C

---
+++ NAMAN05.C
@@ -0,0 +1,19 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() 
+{
+    int a=33;
+    int b=47;
+    int c=20;
+    float average;
+
+
+    average=(a+b+c)/3;
+     printf("\nAverage=%f",
+    average);
+
+    
+
+    return 0;
+}
