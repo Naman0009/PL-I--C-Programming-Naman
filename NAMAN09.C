df2806554087cdb3b606f74a4e532e452e1f5a11
PL-I-C-Programming-Naman <naman0009kumar@gmail.com>
2026-08-31 13:08:37 +0530

    Create NAMAN09.C

---
+++ NAMAN09.C
@@ -0,0 +1,14 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() 
+{
+    //This is a single-line comment 
+     /*
+      This is a multi-line comment 
+      It can span across multiple lines 
+      */
+
+      return 0; 
+    
+}
