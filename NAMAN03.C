2e3a24162434f826d2c75dd73b45f93ef492ab82
PL-I-C-Programming-Naman <naman0009kumar@gmail.com>
2026-08-31 12:56:43 +0530

    Create NAMAN03.C

---
+++ NAMAN03.C
@@ -0,0 +1,13 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() 
+{
+    float radius=1.1;
+    float area;
+
+    area=(3.14*radius*radius);
+     printf("\n Area of a circle=%f",area);
+
+    return 0;
+}
