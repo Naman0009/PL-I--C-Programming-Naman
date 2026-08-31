e69f8ba2fd21bb43de044652933e709f715315ad
PL-I-C-Programming-Naman <naman0009kumar@gmail.com>
2026-08-31 12:51:51 +0530

    Create NAMAN01.C

---
+++ NAMAN01.C
@@ -0,0 +1,24 @@
+Program-1 
+#include <stdio.h>
+
+int main() 
+{
+    int rollnum;
+    float per;
+    char grade;
+
+    rollnum=55;
+    per=88.88;
+    grade='A';
+
+    printf("_____Student Information_____\n");
+    printf("\n Roll Nummber:%d",rollnum);
+    printf("\nPercentage:%f",per);
+    printf("\nGrade   :%c",grade);
+
+
+    return 0;
+
+}
+
+    
